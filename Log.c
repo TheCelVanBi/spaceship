b1d1bdc8a673f2876cf59a745817b8e6524bafc9
ba73 <awdictas@gmail.com>
2019-09-28 17:42:11 +1200

    still working on Log

--- Log.c
+++ Log.c
@@ -3,7 +3,14 @@
 #include "Record.h"
 #include "BattleSimulation.h"
 #include "Spaceship.h"
-
-int log_session() {
+#include "Record.h"
+int init_log(Log self){
+self->ctr=0;
+return 0;
+}
+int log_session(Log self, Record rec) {
 	//todo refactor to match requirements. need to assign spaceship references to battlesimulation instead
+	self->records_array[ctr] = rec;
+	self->ctr++;
+	return 0;
 }
--- Log.h
+++ Log.h
@@ -1,6 +1,7 @@
 typedef struct Log_struct *Log;
 struct Log_struct {
 	Record records_array[100];
+	u_int8_t ctr;
 } Log_struct;
 
-int log_session();
+int log_session(Log self, Record rec);
